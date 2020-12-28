9b932336ab74c44d8e424eeefc7de2ab11c52df3
Kody Fintak <kodyfwee@gmail.com>
2020-12-28 14:59:59 -0800

    Bootstraped from Intellij Cursive.

---
+++ test/encoding_error/core_test.clj
@@ -0,0 +1,7 @@
+(ns encoding-error.core-test
+  (:require [clojure.test :refer :all]
+            [encoding-error.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
